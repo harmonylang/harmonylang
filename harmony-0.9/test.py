dd256968fb04c59de41f4219fc6724077d64ca09
Anthony Yang <ay339@cornell.edu>
2020-09-26 17:11:30 -0400

    Add test runner

---
+++ harmony-0.9/test.py
@@ -0,0 +1,17 @@
+import os
+import subprocess
+
+
+if __name__ == "__main__":
+    pass
+    # stack = [os.path.join(os.path.curdir, "code")]
+    # while stack:
+    #     d = stack.pop()
+    #     print(d)
+    #     for file in os.listdir(d):
+    #         full_path = os.path.join(d, file)
+    #         if file.endswith("hny"):
+    #             os.system(f'python3 harmony.py "{full_path}"')
+    #         if os.path.isdir(full_path):
+    #             print(full_path)
+    #             stack.append(full_path)
