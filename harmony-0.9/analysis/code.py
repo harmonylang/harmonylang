5196ffce6c4e43556360cf0d2535d45caa611476
Anthony Yang <ay339@cornell.edu>
2020-10-19 16:57:57 -0400

    Update documentation

--- harmony-0.9/analysis/code.py
+++ harmony-0.9/analysis/code.py
@@ -5,17 +5,22 @@ from analysis.util import str_of_value
 
 
 class MacroStep:
-    def __init__(self, filename: str, line_number: int, executed_line: str, code: List[Tuple[int, str, str, Optional[int]]]):
+    def __init__(self, filename: str,
+                 line_number: int, executed_line: str, code: List[Tuple[int, str, str, Optional[int]]]):
         self.filename = filename
         self.line_number = line_number
-        self.executed_line = executed_line
+        self.executed_line = executed_line.strip()
         self.first_pc, _, _, _ = min(code, key=lambda p: p[0])
-        self.code = [{'code': c, 'explanation': explanation, 'jump_target': target} for _, c, explanation, target in code]
+        self.code = [{
+            'code': c,
+            'explanation': explanation,
+            'jump_target': target
+        } for _, c, explanation, target in code]
 
     def __str__(self):
         layout: List[str] = [f"{self.filename}:{self.line_number} {self.executed_line}"]
         for pc_off, c in enumerate(self.code):
-            layout.append(f"{pc_off + self.first_pc}: {c[0]}")
+            layout.append(f"{pc_off + self.first_pc}: {c['code']}")
         return "\n".join(layout)
 
     def __repr__(self):
@@ -23,6 +28,15 @@ class MacroStep:
 
 
 def get_code(code, scope, files, typings) -> List[MacroStep]:
+    """
+    Returns a list of the macro-steps run in the program.
+    Based on the htmlcode(code, scope, f) function in harmony.py.
+    :param code:
+    :param scope:
+    :param files:
+    :param typings: The class definitions of types used in Harmony.
+    :return:
+    """
     last_loc = None
     executed_line: Optional[str] = None
     executed_code: List[Tuple[int, str, str, Optional[int]]] = []
@@ -37,17 +51,17 @@ def get_code(code, scope, files, typings) -> List[MacroStep]:
                     executed_line = lines[line - 1]
                 if last_loc is not None:
                     macro_steps.append(MacroStep(filename, line, executed_line, executed_code))
-                    executed_code.clear()
+                    executed_code = []
             last_loc = filename, line
         explanation = code[pc].explain()
         if isinstance(code[pc], typings['JumpOp']) or isinstance(code[pc], typings['JumpCondOp']):
             executed_code.append((pc, str(code[pc]), explanation, code[pc].pc))
         elif isinstance(code[pc], typings['PushOp']) and isinstance(code[pc].constant[0], typings['PcValue']):
-            executed_code.append((pc, f"Push {str_of_value(code[pc].constant[0])}", explanation, code[pc].constant[0].pc))
+            pc_value = code[pc].constant[0].pc
+            executed_code.append((pc, f"Push {str_of_value(code[pc].constant[0])}", explanation, pc_value))
         else:
             executed_code.append((pc, str(code[pc]), explanation, None))
     if last_loc is not None:
         filename, line = last_loc
         macro_steps.append(MacroStep(filename, line, executed_line, executed_code))
-        executed_code.clear()
     return macro_steps
--- harmony-0.9/analysis/path.py
+++ harmony-0.9/analysis/path.py
@@ -48,73 +48,87 @@ class StepValue:
 
 
 def process_steps(steps) -> List[StepValue]:
+    """
+    Returns a list of steps in processes. This is based on the strsteps(steps) function in harmony.py
+    :param steps:
+    :return:
+    """
     if steps is None:
         return []
     result: List[StepValue] = []
     i = 0
     while i < len(steps):
-        (pc, choice) = steps[i]
+        pc, choice = steps[i]
         j = i + 1
         start = pc
         if pc is None:
             result.append(StepValue.make_error("Interrupt"))
+        elif choice is not None:
+            result.append(StepValue.make_choose(start, str_of_value(choice)))
+            pass
         else:
-            if choice is not None:
-                result.append(StepValue.make_choose(start, str_of_value(choice)))
-                pass
+            while j < len(steps):
+                pc2, choice2 = steps[j]
+                if pc is None or pc2 != pc + 1 or choice2 is not None:
+                    break
+                pc, choice = pc2, choice2
+                j += 1
+            if j > i + 1:
+                result.append(StepValue.make_steps((start, pc)))
             else:
-                while j < len(steps):
-                    (pc2, choice2) = steps[j]
-                    if pc is None or pc2 != pc + 1 or choice2 is not None:
-                        break
-                    (pc, choice) = (pc2, choice2)
-                    j += 1
-                if j > i + 1:
-                    result.append(StepValue.make_steps((start, pc)))
-                else:
-                    result.append(StepValue.make_steps((start, start)))
-                pass
+                result.append(StepValue.make_steps((start, start)))
         i = j
     return result
 
 
 def gen_path(n):
-    # Extract the path to node n
+    """
+    Extracts the path to node n
+    """
     path = []
-    while n is not None:
-        if n.after is None:
-            break
-        path = [n] + path
+    while n is not None and n.after is not None:
+        path.insert(0, n)
         n = n.parent
 
     # Now compress the path, combining macrosteps by the same context
     path2 = []
-    lastctx = None
-    laststeps = []
-    laststates = []
-    lastvars = None
+    last_context = None
+    last_steps = []
+    last_states = []
+    last_vars = None
     for n in path:
-        if lastctx is None or lastctx == n.before:
-            laststeps += n.steps
-            lastctx = n.after
-            laststates.append(n.uid)
-            lastvars = n.state.vars
-            continue
-        path2.append((lastctx, laststeps, laststates, lastvars))
-        lastctx = n.after
-        laststeps = n.steps.copy()
-        laststates = [n.uid]
-        lastvars = n.state.vars
-    path2.append((lastctx, laststeps, laststates, lastvars))
+        if last_context is None or last_context == n.before:
+            last_steps += n.steps
+            last_context = n.after
+            last_states.append(n.uid)
+            last_vars = n.state.vars
+        else:
+            path2.append((last_context, last_steps, last_states, last_vars))
+            last_context = n.after
+            last_steps = n.steps.copy()
+            last_states = [n.uid]
+            last_vars = n.state.vars
+    path2.append((last_context, last_steps, last_states, last_vars))
     return path2
 
 
 def get_path(n):
-    # Generate a label for the path table
+    """
+    Returns a dictionary of the processes and steps that led to the node n.
+    {
+        issues: string list;
+        shared_vars: string list;
+        processes: {
+            name: string;
+            sid: int;
+        } list;
+        values_at_process: Map<process_name, Map<variable_name, value_as_string>>;
+        steps: Map<process_name, StepValue_object>
+    }
+    """
     issues = [str(s) for s in n.issues]
-    keys = sorted(n.state.vars.d.keys(), key=key_value)
+    shared_variables = sorted(n.state.vars.d.keys(), key=key_value)
     values_at_process = defaultdict(dict)
-    shared_variables = keys
     path = gen_path(n)
     link_steps: Dict[str, List[StepValue]] = {}
     processes = []
@@ -125,7 +139,7 @@ def get_path(n):
             "name": process_name, "sid": sid
         })
         link_steps[process_name] = process_steps(steps)
-        for k in keys:
+        for k in shared_variables:
             values_at_process[process_name][k] = str(variables.d[k])
     return {
         'issues': issues,
