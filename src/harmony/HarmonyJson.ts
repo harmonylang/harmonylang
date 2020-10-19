5d499014ce2b016a05c85c758ad3581642110de6
Anthony Yang <ay339@cornell.edu>
2020-10-19 17:43:57 -0400

    Update interface

--- src/harmony/HarmonyJson.ts
+++ src/harmony/HarmonyJson.ts
@@ -9,13 +9,19 @@ type HarmonyNodePath = {
   steps: Record<string, unknown[]>
 };
 
+type Code = {
+  "code": string,
+  "explanation": string,
+  "jump_target": number | null
+};
+
 type MacroStep = {
   filename: string;
   line_number: number;
   executed_line: string;
   first_pc: string;
-  code: string[]
-}
+  code: Code[]
+};
 
 type HarmonyNode = {
   uid: number;
@@ -26,12 +32,14 @@ type HarmonyNode = {
 
 class HarmonyJson {
 
-  private json: {
+  private readonly json: {
     bad_node: number | null;
     path_to_bad_node: HarmonyNodePath;
     executed_code: MacroStep[];
     nodes: HarmonyNode[];
   }
+
+  private readonly allCode: Code[];
 
   constructor(filename: PathLike) {
     const file = fs.readFileSync(filename);
@@ -44,10 +52,43 @@ class HarmonyJson {
       executed_code: json.executed_code,
       nodes: json.nodes
     };
+    this.allCode = [];
+    this.json.executed_code.forEach(step => {
+      step.code.forEach(c => {
+        this.allCode.push(c);
+      });
+    });
   }
+
+  hasBadNode(): boolean {
+    return this.json.bad_node != null;
+  }
+
+  getBadNode(): HarmonyNode | null {
+    if (this.json.bad_node != null) {
+      return this.json.nodes[this.json.bad_node];
+    } else {
+      return null;
+    }
+  }
+
+  getIssues(): string[] {
+    return this.json.path_to_bad_node.issues;
+  }
+
+  getAllCode(): Code[] {
+    return this.allCode;
+  }
+
+
 }
 
-const obj = new HarmonyJson("../harmony-0.9/harmony.json");
-
+const obj = new HarmonyJson("../../harmony-0.9/harmony.json");
 
 console.log(obj);
+
+console.log(obj.getBadNode());
+
+console.log(obj.getIssues());
+
+console.log(obj.getAllCode().map(c => c.code).join("\n"));
