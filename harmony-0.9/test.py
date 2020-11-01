01f2981248d9f40d9efa9cec4decd1d0e069e651
Anthony Yang <45056598+ayang4114@users.noreply.github.com>
2020-10-31 21:06:05 -0400

    Update test.py

--- harmony-0.9/test.py
+++ harmony-0.9/test.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     if len(sys.argv) >= 2:
         full_path = sys.argv[1]
     else:
-        full_path = os.path.join(os.path.curdir, "code", "harmony-0.9", "code", "diners.hny")
+        full_path = os.path.join(os.path.curdir, "harmony-0.9", "code", "diners.hny")
     print("Beginning")
     execute(full_path)
     print("Success")
