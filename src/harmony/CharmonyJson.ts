182c0c438a2a2414634b9a96d2983880e10e9a11
Anthony Yang <ay339@cornell.edu>
2021-01-23 14:20:47 -0500

    Reindent files for easier development

--- src/harmony/CharmonyJson.ts
+++ src/harmony/CharmonyJson.ts
@@ -6,71 +6,71 @@ import {getHtmlContent} from "./charm/analysis";
  * @param path
  */
 export const parse = (path: PathLike): HarmonyTopLevel => {
-  return getHtmlContent(path);
+    return getHtmlContent(path);
 };
 
 export type HarmonyTopLevel = {
-  path_to_bad_node: ProcessPathDetail;
-  executed_code: HarmonyCode[];
+    path_to_bad_node: ProcessPathDetail;
+    executed_code: HarmonyCode[];
 };
 
 export type HarmonyCode = {
-  assembly: HarmonyAssembly[];
-  file: string;
-  line: string;
-  source_code: string;
-  start_pc: number;
+    assembly: HarmonyAssembly[];
+    file: string;
+    line: string;
+    source_code: string;
+    start_pc: number;
 };
 
 export type HarmonyAssembly = {
-  code: string;
-  explain: string;
+    code: string;
+    explain: string;
 };
 
 export type ProcessPathDetail = {
-  issue: string;
-  processes: HarmonyProcess[];
-  pid_to_name: Record<string, string>;
+    issue: string;
+    processes: HarmonyProcess[];
+    pid_to_name: Record<string, string>;
 };
 
 export type HarmonyProcess = {
-  pid: string;
-  name: string;
-  slices: HarmonySlice[];
-  duration: number;
-  steps: HarmonyStep[];
+    pid: string;
+    name: string;
+    slices: HarmonySlice[];
+    duration: number;
+    steps: HarmonyStep[];
 }
 
 export type HarmonySlice = {
-  duration: number;
-  shared_values: Record<string, unknown>;
-  trace: Record<string, TraceData>;
-  mode: string;
-  failure: string | null;
+    duration: number;
+    shared_values: Record<string, unknown>;
+    trace: Record<string, TraceData>;
+    mode: string;
+    failure: string | null;
 };
 
 export type HarmonyStep = {
-  choose?: [number, unknown];
-  step?: [number, number];
+    choose?: [number, unknown];
+    step?: [number, number];
 }
 
 export type HarmonyContext = {
-  pid: string;
-  process_name: string;
-  failure: string | null;
-  mode: string;
-  traces: HarmonyTrace[];
+    pid: string;
+    process_name: string;
+    failure: string | null;
+    mode: string;
+    traces: HarmonyTrace[];
 };
 
 export type TraceData = {
-  mode: string;
-  failure: string | null;
-  traces: HarmonyTrace[]
+    mode: string;
+    failure: string | null;
+    traces: HarmonyTrace[]
 };
 
 export type HarmonyTrace = {
-  vars: Record<string, unknown>;
-  method: string;
-  pc: string;
-  calltype: string;
+    vars: Record<string, unknown>;
+    method: string;
+    pc: string;
+    calltype: string;
 };
--- src/outputPanel/PanelController.ts
+++ src/outputPanel/PanelController.ts
@@ -6,118 +6,117 @@ import {createStandaloneHtml} from "../feature/standaloneHtml";
 import {parse} from "../harmony/CharmonyJson";
 
 export default class CharmonyPanelController {
-  public static currentPanel: CharmonyPanelController | undefined;
+    public static currentPanel: CharmonyPanelController | undefined;
+    public static readonly viewType = 'harmonyOutput';
 
-  public static readonly viewType = 'harmonyOutput';
+    private readonly panel: vscode.WebviewPanel;
+    private readonly extensionUri: vscode.Uri;
+    private disposables: vscode.Disposable[] = [];
 
-  private readonly panel: vscode.WebviewPanel;
-  private readonly extensionUri: vscode.Uri;
-  private disposables: vscode.Disposable[] = [];
+    public static createOrShow(extensionUri: vscode.Uri) {
+        const column = vscode.ViewColumn.Beside;
 
-  public static createOrShow(extensionUri: vscode.Uri) {
-    const column = vscode.ViewColumn.Beside;
-
-    // If we already have a panel, show it.
-    if (CharmonyPanelController.currentPanel) {
-      CharmonyPanelController.currentPanel.panel.reveal(column);
-      return;
-    }
-
-    // Otherwise, create a new panel.
-    const panel = vscode.window.createWebviewPanel(
-      CharmonyPanelController.viewType,
-      'Harmony Output',
-      column,
-      {
-        // Enable javascript in the webview
-        enableScripts: true,
-        localResourceRoots: [vscode.Uri.file(path.join(__dirname, '..', '..', 'resource'))]
-      }
-    );
-    CharmonyPanelController.currentPanel = new CharmonyPanelController(panel, extensionUri);
-  }
-
-  public static revive(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
-    CharmonyPanelController.currentPanel = new CharmonyPanelController(panel, extensionUri);
-  }
-
-  private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
-    this.panel = panel;
-    this.extensionUri = extensionUri;
-
-    // Set the resource-view's initial html content
-    this.update();
-
-    // Listen for when the panel is disposed
-    // This happens when the user closes the panel or when the panel is closed programatically
-    this.panel.onDidDispose(() => this.dispose(), null, this.disposables);
-  }
-
-  public updateResults() {
-    // Send a message to the webview webview.
-    // You can send any JSON serializable data.
-    console.log("Presenting the webview");
-    this.update(true);
-  }
-
-  public updateMessage(message: string) {
-    const webview = this.panel.webview;
-    webview.postMessage({ command: 'message', jsonData: message });
-  }
-
-  public dispose() {
-    CharmonyPanelController.currentPanel = undefined;
-
-    // Clean up our resources
-    this.panel.dispose();
-
-    while (this.disposables.length) {
-      const x = this.disposables.pop();
-      if (x) {
-        x.dispose();
-      }
-    }
-  }
-
-  private update(hasData = false) {
-    const webview = this.panel.webview;
-    const harmonyPanel = this.panel;
-
-    const uiPath = path.join(__dirname, '..', '..', 'resource', 'charmony.html');
-    const dataPath = path.join(__dirname, '..', '..', 'resource', 'charm.json');
-    if (!hasData){
-      console.log("Looking for data");
-      fs.readFile(uiPath, 'utf-8', function (err, data) {
-        if (err) {
-          console.log(err);
-          vscode.window.showInformationMessage(err.message);
-        } else {
-          harmonyPanel.webview.html = data;
+        // If we already have a panel, show it.
+        if (CharmonyPanelController.currentPanel) {
+            CharmonyPanelController.currentPanel.panel.reveal(column);
+            return;
         }
-      });
-    }
-    if (hasData){
-      console.log(uiPath, dataPath);
-      this.loadData(dataPath, webview);
-    }
-  }
 
-  private loadData(dataPath: string, webview: Webview) {
-    console.log("Step 1");
-    const jsonData = parse(dataPath);
-    console.log("Step 2");
-    if (vscode.workspace.workspaceFolders) {
-      console.log("Creating a standalone HTML file");
-      createStandaloneHtml(vscode.workspace.workspaceFolders[0].uri.path, jsonData);
+        // Otherwise, create a new panel.
+        const panel = vscode.window.createWebviewPanel(
+            CharmonyPanelController.viewType,
+            'Harmony Output',
+            column,
+            {
+                // Enable javascript in the webview
+                enableScripts: true,
+                localResourceRoots: [vscode.Uri.file(path.join(__dirname, '..', '..', 'resource'))]
+            }
+        );
+        CharmonyPanelController.currentPanel = new CharmonyPanelController(panel, extensionUri);
     }
-    console.log(jsonData);
-    webview.postMessage({ command: 'load', jsonData });
-    webview.onDidReceiveMessage( message => {
-      switch (message.command) {
-        case 'alert':
-          vscode.window.showErrorMessage(message.text);
-          return;
-      }
-    }, undefined, undefined);
-  }
+
+    public static revive(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+        CharmonyPanelController.currentPanel = new CharmonyPanelController(panel, extensionUri);
+    }
+
+    private constructor(panel: vscode.WebviewPanel, extensionUri: vscode.Uri) {
+        this.panel = panel;
+        this.extensionUri = extensionUri;
+
+        // Set the resource-view's initial html content
+        this.update();
+
+        // Listen for when the panel is disposed
+        // This happens when the user closes the panel or when the panel is closed programatically
+        this.panel.onDidDispose(() => this.dispose(), null, this.disposables);
+    }
+
+    public updateResults() {
+        // Send a message to the webview webview.
+        // You can send any JSON serializable data.
+        console.log("Presenting the webview");
+        this.update(true);
+    }
+
+    public updateMessage(message: string) {
+        const webview = this.panel.webview;
+        webview.postMessage({ command: 'message', jsonData: message });
+    }
+
+    public dispose() {
+        CharmonyPanelController.currentPanel = undefined;
+
+        // Clean up our resources
+        this.panel.dispose();
+
+        while (this.disposables.length) {
+            const x = this.disposables.pop();
+            if (x) {
+                x.dispose();
+            }
+        }
+    }
+
+    private update(hasData = false) {
+        const webview = this.panel.webview;
+        const harmonyPanel = this.panel;
+
+        const uiPath = path.join(__dirname, '..', '..', 'resource', 'charmony.html');
+        const dataPath = path.join(__dirname, '..', '..', 'resource', 'charm.json');
+        if (!hasData){
+            console.log("Looking for data");
+            fs.readFile(uiPath, 'utf-8', function (err, data) {
+                if (err) {
+                    console.log(err);
+                    vscode.window.showInformationMessage(err.message);
+                } else {
+                    harmonyPanel.webview.html = data;
+                }
+            });
+        }
+        if (hasData){
+            console.log(uiPath, dataPath);
+            this.loadData(dataPath, webview);
+        }
+    }
+
+    private loadData(dataPath: string, webview: Webview) {
+        console.log("Step 1");
+        const jsonData = parse(dataPath);
+        console.log("Step 2");
+        if (vscode.workspace.workspaceFolders) {
+            console.log("Creating a standalone HTML file");
+            createStandaloneHtml(vscode.workspace.workspaceFolders[0].uri.path, jsonData);
+        }
+        console.log(jsonData);
+        webview.postMessage({ command: 'load', jsonData });
+        webview.onDidReceiveMessage( message => {
+            switch (message.command) {
+                case 'alert':
+                    vscode.window.showErrorMessage(message.text);
+                    return;
+            }
+        }, undefined, undefined);
+    }
 }
