bafcc70521339406301a6243e86a3810ef3c5a16
Anthony Yang <ay339@cornell.edu>
2021-09-13 10:53:10 -0400

    remove boilerplate completion resolve code

--- src/server/server.ts
+++ src/server/server.ts
@@ -223,13 +223,6 @@ connection.onCompletion(
 // the completion list.
 connection.onCompletionResolve(
     (item: CompletionItem): CompletionItem => {
-        if (item.data === 1) {
-            item.detail = 'TypeScript details';
-            item.documentation = 'TypeScript documentation';
-        } else if (item.data === 2) {
-            item.detail = 'JavaScript details';
-            item.documentation = 'JavaScript documentation';
-        }
         return item;
     }
 );
